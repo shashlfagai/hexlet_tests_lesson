0e08dedad69ddddf2d22d7c58db12fbd6f9d8d03
Loginov <itsmeegorloginov@gmail.com>
2023-06-16 14:43:14 +0300

    now assert using for tests

--- tests/test_capitalize.py
+++ tests/test_capitalize.py
@@ -1,8 +1,6 @@
 from capitalize import capitalize
 
-if capitalize('hello') != 'Hello':
-    raise Exception("Функция работает неверно!")
-if capitalize('') != '':
-    raise Exception("Функция работает неверно!")
+assert capitalize('hello') == "Hello"
+assert capitalize('') == ''
 print ("Вспе тесты пройдены! Все работает ништяк!")
 
